44c4eb352e05c4a9a21d5299777b385191f9111b
Bekim Gjokaj <bekim.gjokaj@medmen.com>
2018-10-16 08:35:33 -0700

    Do it embed image

--- src/Services/CommandHandler.cs
+++ src/Services/CommandHandler.cs
@@ -1,8 +1,9 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
-using System.Threading.Tasks;
 using System;
+using System.Threading.Tasks;
 
 namespace Luci
 {
@@ -30,27 +31,38 @@ namespace Luci
 
             _discord.MessageReceived += OnMessageReceivedAsync;
         }
-        
+
         private async Task OnMessageReceivedAsync(SocketMessage s)
         {
-            var msg = s as SocketUserMessage;     // Ensure the message is from a user/bot
-            if (msg == null) return;
-            if (msg.Author.Id == _discord.CurrentUser.Id) return;     // Ignore self when checking commands
-            
-            var context = new SocketCommandContext(_discord, msg);     // Create the command context
+            SocketUserMessage msg = s as SocketUserMessage;     // Ensure the message is from a user/bot
+            if (msg == null)
+            {
+                return;
+            }
+
+            if (msg.Author.Id == _discord.CurrentUser.Id)
+            {
+                return;     // Ignore self when checking commands
+            }
+
+            SocketCommandContext context = new SocketCommandContext(_discord, msg);     // Create the command context
 
             int argPos = 0;     // Check if the message has a valid command prefix
             if (msg.HasStringPrefix(_config["prefix"], ref argPos) || msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
             {
-                var result = await _commands.ExecuteAsync(context, argPos, _provider);     // Execute the command
+                IResult result = await _commands.ExecuteAsync(context, argPos, _provider);     // Execute the command
 
                 if (!result.IsSuccess)     // If not successful, reply with the error.
+                {
                     await context.Channel.SendMessageAsync(result.ToString());
+                }
             }
 
-            if(msg.Content.ToLower().Contains("should i"))
+            if (msg.Content.ToLower().Contains("should i"))
             {
-                 await context.Channel.SendMessageAsync("Do it. Do it. Do it.");
+                var eb = new EmbedBuilder() { ImageUrl= "https://media.giphy.com/media/deDsaGovR3BMiNh45V/giphy.gif" };
+                await context.Channel.SendMessageAsync("", false, eb);
+                
             }
         }
     }
